2ce2029ff89488940de8abdcafbb62a64fda4393
Markus Schulz <schulz@alpharesearch.de>
2017-02-07 15:19:14 -0500

    action bar with print button

--- FoodPrinter/app/src/main/java/de/alpharesearch/printer/MainActivity.java
+++ FoodPrinter/app/src/main/java/de/alpharesearch/printer/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.StrictMode;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
@@ -19,9 +22,60 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
         TextView mTextView = (TextView)this.findViewById(R.id.textView_Preview);
-        mTextView.setText("Enter the information and press Print.");
+        mTextView.setText(R.string.Enter_the_information_and_press_Print);
 
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean Print(MenuItem item) {
+        EditText editTextIP = (EditText) this.findViewById(R.id.editTextIP);
+        EditText editText_Measurement = (EditText) this.findViewById(R.id.editText_Size);
+        int measurement  = 0;
+        try {
+            measurement = Integer.parseInt(editText_Measurement.getText().toString());
+        }
+        catch(NumberFormatException nfe) {
+            Log.e(String.valueOf(R.string.Print),"trying to convert:"+editText_Measurement.getText().toString()+" to integer failed");
+            measurement  = 0;
+        }
+        EditText editText_Servings = (EditText) this.findViewById(R.id.editText_Servings);
+        int servings  = 0;
+        try {
+            servings = Integer.parseInt(editText_Servings.getText().toString());
+        }
+        catch(NumberFormatException nfe) {
+            Log.e(String.valueOf(R.string.Print),"trying to convert:"+editText_Servings.getText().toString()+" to integer failed");
+            servings  = 1;
+        }
+        EditText editText_Unit = (EditText) this.findViewById(R.id.editText_Unit);
+        EditText editText_Comment = (EditText) this.findViewById(R.id.editText_Comment);
+        TextView mTextView_Preview = (TextView) this.findViewById(R.id.textView_Preview);
+        String print_this = "\nPortion Information\nTotal measurement: "+Integer.toString(measurement)+editText_Unit.getText().toString()+"\nServings: "+Integer.toString(servings)+"\nPortion Size: "+Integer.toString((measurement/servings))+editText_Unit.getText().toString()+"\nCommnet: "+editText_Comment.getText().toString()+"\n\n\n";
+
+        mTextView_Preview.setText(print_this);
+        new ConnectTask().execute(editTextIP.getText().toString());
+        SystemClock.sleep(1000);
+        if (mTcpClient != null) {
+            mTcpClient.sendMessage(print_this);
+        }
+        SystemClock.sleep(1000);
+        if (mTcpClient != null) {
+            mTcpClient.stopClient();
+        }
+        return true;
     }
 
     public void Print(View v) {
@@ -33,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
             measurement = Integer.parseInt(editText_Measurement.getText().toString());
         }
         catch(NumberFormatException nfe) {
-            Log.e("Print","trying to convert:"+editText_Measurement.getText().toString()+" to integer failed");
+            Log.e(String.valueOf(R.string.Print),"trying to convert:"+editText_Measurement.getText().toString()+" to integer failed");
             measurement  = 0;
         }
         EditText editText_Servings = (EditText) this.findViewById(R.id.editText_Servings);
@@ -42,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
             servings = Integer.parseInt(editText_Servings.getText().toString());
         }
         catch(NumberFormatException nfe) {
-            Log.e("Print","trying to convert:"+editText_Servings.getText().toString()+" to integer failed");
+            Log.e(String.valueOf(R.string.Print),"trying to convert:"+editText_Servings.getText().toString()+" to integer failed");
             servings  = 1;
         }
         EditText editText_Unit = (EditText) this.findViewById(R.id.editText_Unit);
@@ -85,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
             //response received from server
-            Log.d("test", "response " + values[0]);
+            Log.d("received", "response " + values[0]);
             //process server response here....
 
         }
